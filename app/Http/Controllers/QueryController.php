fd81242c5bc78abb16021f483b0fdab4b91553f8
foysal41 <mdfoysal.mf33@gmail.com>
2024-07-08 21:13:24 +0600

    orderby, groupby , যে বইটার দাম maximum তা বের করা.

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -8,18 +8,26 @@ use Illuminate\Support\Facades\DB;
 class QueryController extends Controller
 {
    function index(){
-    //Where book id >=3 and <= 7
-    $books = DB::table('books')->whereBetween('id' ,[3, 7])->get();
+    
 
-    // Different Version of between
-    $books = DB::table('books')
-    ->where('id', '>=', 3)
-    ->where('id', '<=', 7)
-    ->get();
+   // Maximum price retrive from books
+        // $max = DB::table('books')->max('price');
+        // return response($max);
 
-    //Where id=3 or id =7 | Only show  id 3 and 7
-    $books = DB::table('books')->whereIn('id', [3, 7])->get();
+    // Find the book with maximum price | Higest Price Book
+        $max = DB::table('books')->max('price');
+        $maxPriceBook = DB::table('books')->wherePrice($max)->get();
+        return response()->json($maxPriceBook);
 
-    return response($books);
+    // Order by with book name use title | Start from 5,4, 3, 2, 1
+        $books = DB::table('books')->orderBy('title')->get();
+        return response()->json($books);
+        
+
+    // Order by with book name use title
+        $books = DB::table('books')->orderBy('title' , 'desc')->get();
+        return response()->json($books);
+
+
    }
 }
