5aa7e3fcddd364fd01b75034f4694e42ddc4b53b
foysal41 <mdfoysal.mf33@gmail.com>
2024-07-08 18:46:44 +0600

    রেকর্ড তুলে আনার জন্য

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -8,10 +8,16 @@ use Illuminate\Support\Facades\DB;
 class QueryController extends Controller
 {
     function index(){
-       $books= DB::table('books')->get();
-    
+        //important হচ্ছে author বা books থেকে important রেকর্ড তুলে আনতে পারি। এবং বিভিন্ন টেবিল থাকে join  করে রেকর্ড তুলে আনতে পারি
 
-       $count = DB::table('books')->count();
-       return response()->json($count);
+        //"এখানে প্রথম দুইটা রেকর্ড তুলে আনতে চাইলে লিমিট(২) ব্যবহার হয়েছে "
+
+       $authors = DB::table('authors')->limit(2)->get();
+
+       //"এবার যদি বলি প্রথম দুইটা পর থাকে পরের দুইটা তুলে আনো "
+       
+       $authors = DB::table('authors')->limit(3)->offset(2)->get();
+
+       return response()->json($authors);
     }
 }
