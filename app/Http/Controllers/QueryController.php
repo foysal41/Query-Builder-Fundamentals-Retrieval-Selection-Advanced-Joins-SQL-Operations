88a269593b89e6b866d3ad17be75b2f36052a7b2
foysal41 <mdfoysal.mf33@gmail.com>
2024-07-08 18:24:47 +0600

    setup controller,migrations_tablel,seeds, db_connection, finally in controller retrive  data from books with json

---
+++ app/Http/Controllers/QueryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class QueryController extends Controller
+{
+    function index(){
+       $books= DB::table('books')->get();
+       return response()->json($books);
+       
+    }
+}
