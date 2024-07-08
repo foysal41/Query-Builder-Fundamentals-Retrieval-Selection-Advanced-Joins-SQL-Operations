1b50e0ef415938a73b7f0186b1f89953d7e5cb19
Foysal <foysal>
2024-07-09 02:36:04 +0600

    টেবিলের মধ্যে জয়েন তৈরি and ক নমবর authors কতগল বই আছ

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -10,24 +10,29 @@ class QueryController extends Controller
    function index(){
     
 
-   // Maximum price retrive from books
-        // $max = DB::table('books')->max('price');
-        // return response($max);
+    $books = DB::table('books')
 
-    // Find the book with maximum price | Higest Price Book
-        $max = DB::table('books')->max('price');
-        $maxPriceBook = DB::table('books')->wherePrice($max)->get();
-        return response()->json($maxPriceBook);
+    /*
+    একটা টেবিলের সাথে আরেকটা টেবিলের মধ্যে জয়েন তৈরি করতে হলে. 
+    ->join('authors', 'books.author_id', '=' , 'authors.id')
+    এর মানে হচ্ছে: 
+    'authors' (টেবিলের মধ্যে একটি জয়েন অপারেশন করে।)
+    books.author_id ((ডট) চিহ্নটি টেবিল এবং কলামের মধ্যে একটি সম্পর্ক স্থাপন করে।)
+    '=' (এখানে, 'books' টেবিলের 'author_id' কলামের মান এবং 'authors' টেবিলের 'id' কলামের মান সমান হতে হবে। )
+    ‘authors.id’ = (টেবিলের কোনো লেখকের 'id' এর সাথে মেলে সেই বই এবং লেখকের তথ্য একত্রে পাওয়া যাবে।)
 
-    // Order by with book name use title | Start from 5,4, 3, 2, 1
-        $books = DB::table('books')->orderBy('title')->get();
-        return response()->json($books);
-        
+    */ 
 
-    // Order by with book name use title
-        $books = DB::table('books')->orderBy('title' , 'desc')->get();
-        return response()->json($books);
+    ->join('authors', 'books.author_id', '=' , 'authors.id')
+    ->select('books.title', 'authors.name as author_name', 'books.author_id', 'books.id')
+    //দেখতে চাই এক নাম্বার authors কতগুলো বই আছে 
+    ->where('authors.id', 1)
+    ->get();
 
+    return response()->json($books);
+
+
+    
 
    }
 }
